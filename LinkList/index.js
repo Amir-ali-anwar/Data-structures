1e591add6ed238e51f115dd05b95a4589cbbbb10
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-08-19 19:12:15 +0500

    added the append method

--- LinkList/index.js
+++ LinkList/index.js
@@ -15,22 +15,51 @@ class LinkedList {
     getSize() {
         return this.size
     }
-    prepend(value){
-        const node= new Node(value)
-        if(this.isEmpty()){
-            this.head=node
-        }else{
-            node.next=this.head
-            this.head= node
+    // Prepend has the constant time complexity o(1)
+    prepend(value) {
+        const node = new Node(value)
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            node.next = this.head
+            this.head = node
         }
         this.size++
+    }
+    // Prepend has the constant time complexity o(n)
+    append(value){
+        const node= new Node(value);
+        if(this.isEmpty()){
+           this.head=node 
+        }else{
+            let prev= this.head;
+            while (prev.next) {
+                prev= prev.next
+            }
+            prev.next= node
+        }
+        this.size++
+    }
+    print() {
+        let curr = this.head;
+        let storedValues = ''
+        while (curr) {
+            storedValues += `${curr.value} `
+            curr = curr.next 
+        }
+        console.log({storedValues})
     }
 }
 const list = new LinkedList()
 console.log('====================================');
-console.log("list empty", list.getSize());
+console.log("list size", list.getSize());
 console.log("list empty", list.isEmpty());
+list.print()
+list.prepend(60)
 list.prepend(10)
 list.prepend(40)
-list.prepend(60)
+list.prepend(0)
+list.append(100)
+list.append(101)
+list.print()
 console.log('====================================');
