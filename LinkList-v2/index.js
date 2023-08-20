ce239d2eb201e510e36140dbf39bdc82b09010be
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-08-20 14:59:20 +0500

    added remove at function

--- LinkList-v2/index.js
+++ LinkList-v2/index.js
@@ -29,6 +29,7 @@ class LinkedList {
         this.size++
     }
     printListData() {
+
         let current = this.head
         while (current) {
             console.log(current.data);
@@ -36,8 +37,35 @@ class LinkedList {
         }
 
     }
+    removeAt(index) {
+        if (index < 0 && index > this.size) {
+            return;
+        }
+        let current = this.head
+        let previous
+        let count=0;
+        if(index===0){
+           this.head= current.next;
+
+        }else{
+            while(count < index){
+                count++;
+                previous = current;
+                current = current.next;
+            }
+            previous.next=current.next
+        }
+        this.size--
+
+    }
+    clearList() {
+        this.head = null;
+        this.size = 0;
+    }
 }
 const ll = new LinkedList();
 ll.insertFirst(1)
 ll.insertLast(123)
+ll.printListData()
+ll.clearList()
 ll.printListData()
