651b48a123d14694aacbd397ba909a6c21b4a69d
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-08-20 15:03:39 +0500

    added insertAt functionn

--- LinkList-v2/index.js
+++ LinkList-v2/index.js
@@ -43,20 +43,46 @@ class LinkedList {
         }
         let current = this.head
         let previous
-        let count=0;
-        if(index===0){
-           this.head= current.next;
+        let count = 0;
+        if (index === 0) {
+            this.head = current.next;
 
-        }else{
-            while(count < index){
+        } else {
+            while (count < index) {
                 count++;
                 previous = current;
                 current = current.next;
             }
-            previous.next=current.next
+            previous.next = current.next
         }
         this.size--
 
+    }
+    insertAt(data, index){
+         if (index < 0 && index > this.size) {
+            return;
+        }
+        if (index === 0) {
+            this.insertFirst(data);
+            return;
+          }else{
+            let node= new Node(data)
+            let current,previous;
+            current = this.head;
+            let count = 0;
+            while (count < index) {
+                previous = current; // Node before index
+                count++;
+                current = current.next; // Node after index
+              }
+          
+              node.next = current;
+              previous.next = node;
+          
+              this.size++;
+
+          }
+      
     }
     clearList() {
         this.head = null;
