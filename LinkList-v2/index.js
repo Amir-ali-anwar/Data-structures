58c2da100aa628c8e1672bf6adad3848bb098a07
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-08-20 13:17:55 +0500

    insertFirst function & insertLast

--- LinkList-v2/index.js
+++ LinkList-v2/index.js
@@ -1,13 +1,31 @@
 // create the single node
 class Node {
-    constructor(data,next=null){
-        this.data=data
-        this.next=next
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
     }
 }
-class LinkedList{
-    constructor(){
-        this.head=null;
-        this.size=0;
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    insertFirst(data) {
+        this.head = new Node(data, this.head)
+        this.size++
+    }
+    insertLast(data) {
+        let node = new Node(data)
+        let current = ''
+        if (!this.head) {
+            this.head = node;
+        } else {
+            current = this.head
+            while (current.head) {
+                current = current.next
+            }
+            current.next = node
+        }
+        this.size++
     }
 }
