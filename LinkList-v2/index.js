a66233b5c085f292faf9c1ade0ab384dcc0f587b
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-08-20 12:38:37 +0500

    added Single Node and linkedlist class

---
+++ LinkList-v2/index.js
@@ -0,0 +1,13 @@
+// create the single node
+class Node {
+    constructor(data,next=null){
+        this.data=data
+        this.next=next
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head=null;
+        this.size=0;
+    }
+}
