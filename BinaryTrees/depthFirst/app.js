de36004dc0271bc42564caeef5771d7ab2f8f709
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-09-02 15:36:06 +0500

    depthFirst values

--- BinaryTrees/depthFirst/app.js
+++ BinaryTrees/depthFirst/app.js
@@ -1,37 +1,13 @@
-// const depthFirstPrint = (graph, source) => {
-//   //   console.log(graph);
-//   const visited = new Set();
-//   const stack = [source];
-//   while (stack.length > 0) {
-//     const current = stack.pop();
-//     console.log(current);
-//     if(!visited.has(current)){
-//         for (let neighbor of graph[current]) {
-//             if (!visited.has(neighbor)) {
-//               stack.push(neighbor);
-//             }
-//           }
-//     }
-//   }
-// };
-
-
-const depthFirstPrint=(graph,source)=>{
-  for (let neighbor of graph[source]) {
-    depthFirstPrint(graph,neighbor)
-      console.log(neighbor);
+const depthFirstValues = (root) => {
+  if (!root) return [];
+  const result = [];
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    result.push(current);
+    if (current.right) stack.push(current.right);
+    if (current.left) stack.push(current.left);
+    return result
   }
-}
-
-
-const graph = {
-    a: ["b", "c"],
-    b: ["d"],
-    c: ['e'], // Fixed the graph data, c has no neighbors
-    d: ["f"],
-    e: [],
-    f: [],
-  };
-  
-depthFirstPrint(graph, "a");
-
+  return result;
+};
