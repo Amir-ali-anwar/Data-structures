7b5b9efac8f59cb46c45d75b2008d2e50bca6a77
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-09-02 15:44:26 +0500

    depthfirst example completed by the recursion

--- BinaryTrees/depthFirst/app.js
+++ BinaryTrees/depthFirst/app.js
@@ -1,13 +1,21 @@
+// const depthFirstValues = (root) => {
+//   if (!root) return [];
+//   const result = [];
+//   const stack = [root];
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     result.push(current);
+//     if (current.right) stack.push(current.right);
+//     if (current.left) stack.push(current.left);
+//     return result
+//   }
+//   return result;
+// };
+// depth values by resursion
+
 const depthFirstValues = (root) => {
   if (!root) return [];
-  const result = [];
-  const stack = [root];
-  while (stack.length > 0) {
-    const current = stack.pop();
-    result.push(current);
-    if (current.right) stack.push(current.right);
-    if (current.left) stack.push(current.left);
-    return result
-  }
-  return result;
+  const leftValues = depthFirstValues(root.left);
+  const rightValues = depthFirstValues(root.right);
+  return [root.val, ...leftValues, ...rightValues];
 };
