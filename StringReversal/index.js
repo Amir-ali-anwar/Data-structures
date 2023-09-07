e986301fe79795b113f5e9b25bc39703c2ddc0c0
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-09-07 17:59:30 +0500

    added the string reverse methods

--- StringReversal/index.js
+++ StringReversal/index.js
@@ -7,4 +7,31 @@ for (let index = str.length-1; index>=0; index --) {
 }
 return reversed
 }
-console.log(reverseString(str));
+// console.log(reverseString(str));
+
+const stringReversal=(str)=>{
+   return str.split('').reverse().join('') 
+    
+}
+
+// console.log(stringReversal(str));
+
+const reverseThree=(str)=>{
+    let reversed= ''
+    for (const iterator of str) {
+        reversed= iterator+reversed
+        console.log(reversed);
+    }
+   console.log(reversed);
+}
+
+
+// reverseThree(str)
+
+const reverseByReduce = (str) => {
+  return (reversed = str.split("").reduce((acc, curr) => {
+    return (curr += acc);
+  }, ""));
+};
+
+console.log(reverseByReduce(str))
