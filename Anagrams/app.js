b734f499e11f1de8a475c8fadc9e34d4777abf34
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-10-01 09:28:32 +0500

    Anagrams examples

--- Anagrams/app.js
+++ Anagrams/app.js
@@ -14,20 +14,46 @@ const anagram = (strA, strB) => {
     }
     for (const i in CleanStrB) {
       if (!obj2[CleanStrB[i]]) {
-        obj1[CleanStrB[i]] = 1;
+        obj2[CleanStrB[i]] = 1;
       } else {
         obj2[CleanStrB[i]] += 1;
       }
     }
   }
-  const keysA= Object.keys(obj1)
-  const keysB= Object.keys(obj2)
+  const keysA = Object.keys(obj1)
+  const keysB = Object.keys(obj2)
   console.log(keysA.length);
   console.log(keysB.length);
-  if(keysA.length !== keysB.length ){
+  if (keysA.length !== keysB.length) {
     return false
-  } 
+  }
   return true
 };
 
-console.log(anagram("Dormitory", "Dirty Room"));
+// console.log(anagram("Dormitory", "Dirty Room"));
+
+
+// solution 2
+
+const Anagram = (strA, strB) => {
+  const char1= characterMap(strA)
+  const char2= characterMap(strB)
+  if (Object.keys(char1).length !== Object.keys(char2).length) {
+    return false
+  }
+  for (const key in char1) {
+    if(char1[key] !== char2[key]){
+      return false
+    }
+  }
+  return true
+}
+
+const characterMap = (str) => {
+  const obj = {}
+  for (let index of str.replace(/[^\w]/g, "").toLowerCase()) {
+    obj[index] = obj[index] + 1 || 1
+  }
+  return obj
+}
+console.log(Anagram("Dormitory", "Dirty Room"));
