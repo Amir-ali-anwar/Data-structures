e0bca53a1bc2c25d90d00d331f5b65c292cb4404
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-12-31 15:27:26 +0500

    understanding the strings by solving problems

--- StringReversal/index.js
+++ StringReversal/index.js
@@ -1,28 +1,28 @@
 const str = 'jadu';
 
-const reverseString=(str)=>{
-let reversed= ""
-for (let index = str.length-1; index>=0; index --) {
-    reversed= reversed + str[index]
-}
-return reversed
+const reverseString = (str) => {
+  let reversed = ""
+  for (let index = str.length - 1; index >= 0; index--) {
+    reversed = reversed + str[index]
+  }
+  return reversed
 }
 // console.log(reverseString(str));
 
-const stringReversal=(str)=>{
-   return str.split('').reverse().join('') 
-    
+const stringReversal = (str) => {
+  return str.split('').reverse().join('')
+
 }
 
 // console.log(stringReversal(str));
 
-const reverseThree=(str)=>{
-    let reversed= ''
-    for (const iterator of str) {
-        reversed= iterator+reversed
-        console.log(reversed);
-    }
-   console.log(reversed);
+const reverseThree = (str) => {
+  let reversed = ''
+  for (const iterator of str) {
+    reversed = iterator + reversed
+    console.log(reversed);
+  }
+  console.log(reversed);
 }
 
 
@@ -35,3 +35,36 @@ const reverseByReduce = (str) => {
 };
 
 console.log(reverseByReduce(str))
+
+
+
+
+// Strings reversal revision
+
+const strr = 'test';
+
+// method-1
+
+const strReverse = (str) => {
+  let reversed = '';
+  for (let index = str.length - 1; index >= 0; index--) {
+    reversed = reversed + str[index];
+  }
+  return reversed
+}
+
+console.log(strReverse(strr));
+
+
+// method-2
+
+const strReverse2=(str)=>{
+  return str.split('').reduce((acc,curr)=>{
+    return curr + acc
+  },'')
+}
+console.log(strReverse2(strr));
+
+
+
+
