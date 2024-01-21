7e4be50f749b2a3052b70e97a2d03e89abf5fb49
Amir Ali anwar <amiralianwar611@gmail.com>
2024-01-21 15:55:18 +0500

    updated the getsize fb

--- Lnklist/SinglyLinkList/app.js
+++ Lnklist/SinglyLinkList/app.js
@@ -8,10 +8,20 @@ class Node {
 class LinkList {
   constructor() {
     this.head = null
+
   }
   insertFirst(data) {
-    this.head = new Node(data,)
+    this.head = new Node(data,this.head)
 
+  }
+  getSize() {
+    let count = 0;
+    let current = this.head
+    while (current) {
+      count++
+      current = current.next
+    }
+    return count
   }
   insertAtEnd(data) {
     const dataNode = new Node(data)
@@ -25,10 +35,53 @@ class LinkList {
     }
     currentNode.next = dataNode
   }
+  insertAt(data, index) {
+    if (index < 0 || index > this.getSize()) {
+      return;
+    }
+    if (index === 0 || !this.head) {
+      this.insertFirst(data)
+      return;
+    } else {
+      let newNode = new Node(data)
+      const currentNode = this.head
+      let count = 0
+      while (count < index - 1 && count.next) {
+        currentNode = currentNode.next
+        count++
+      }
+      newNode.next = currentNode.next
+      currentNode.next = newNode
+    }
+  }
+  clearList() {
+    this.head = null
+  }
+  isEmpty() {
+    return this.getSize() === 0
+  }
+  printList(){
+    if(this.isEmpty()){
+      console.log("List is empty");
+      throw new Error("List is empty")
+    }else{
+      let curr= this.head;
+      let list=''
+      while (curr) {
+        list += `${curr.data}`;
+        curr = curr.next;
+      }
+      return list
+    }
+  }
 }
 
 const firstObj = new LinkList()
 
 firstObj.insertFirst(1)
-firstObj.insertAtEnd(12)
-console.log(firstObj);
+// firstObj.insertAtEnd(12)
+// firstObj.insertAt(12, 3)
+// firstObj.isEmpty()
+// firstObj.isEmpty()
+console.log(firstObj.getSize());
+console.log(firstObj.printList());
