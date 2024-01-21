b680cb9411c93e3ba0332fcc058dc51505542ac2
Amir Ali anwar <amiralianwar611@gmail.com>
2024-01-21 12:36:35 +0500

    linklist

--- Lnklist/SinglyLinkList/app.js
+++ Lnklist/SinglyLinkList/app.js
@@ -1,21 +1,34 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data
-        this.next = next
-    }
+  constructor(data, next = null) {
+    this.data = data
+    this.next = next
+  }
 }
 
 class LinkList {
-    constructor() {
-        this.head = null
-    }
-    insertFirst(data) {
-        this.head = new Node(data,)
+  constructor() {
+    this.head = null
+  }
+  insertFirst(data) {
+    this.head = new Node(data,)
 
+  }
+  insertAtEnd(data) {
+    const dataNode = new Node(data)
+    if (!this.head) {
+      this.head = dataNode
+      return;
     }
+    const currentNode = this.head
+    while (currentNode.next) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = dataNode
+  }
 }
 
 const firstObj = new LinkList()
 
 firstObj.insertFirst(1)
+firstObj.insertAtEnd(12)
 console.log(firstObj);
