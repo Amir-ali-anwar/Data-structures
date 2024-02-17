893ec12a1158e1190ba3dd44ec45b629dd3c7b9f
Amir Ali anwar <amiralianwar611@gmail.com>
2024-02-17 18:39:14 +0500

    Search method for BST

--- BST/app.js
+++ BST/app.js
@@ -1,30 +1,63 @@
 class Node {
-    constructor(value) {
-        this.value = value
-        this.left = null
-        this.right = null
-    }
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
 }
 class BinarySearchTree {
-    constructor() {
-        this.root=null
+  constructor() {
+    this.root = null
+  }
+  isEmpty() {
+    return this.root === null
+  }
+  insert(value) {
+    const newNode = new Node(value)
+    if (this.root===null) {
+      this.root = newNode
+    } else {
+      this.insertNode(this.root, newNode)
     }
-    isEmpty(){
-        return this.root===null
+  }
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode
+      } else {
+        this.insertNode(root.left, newNode)
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode
+      } else {
+        this.insertNode(root.right, newNode)
+      }
     }
-    insert(value) {
-        const newNode = new Node(value)
-        if (this.root) {
-            this.root = newNode
-        }else{
-            this.insertNode(this.root,newNode)
-        }
+  }
+  search(root, value) {
+    if (!root) {
+      return false
     }
-    insertNode(root,newNode){
-
+    
+    else {
+      if (root.value === value) {
+        return true
+      } else if (value < root.value) {
+        return this.search(root.left, value)
+      } else {
+        return this.search(root.right, value)
+      }
     }
+  }
 }
 
-const BST= new BinarySearchTree()
+const BST = new BinarySearchTree()
 
 console.log('Tree is Empty', BST.isEmpty());
+
+BST.insert(12)
+BST.insert(13)
+BST.insert(14)
+
+console.log(BST.search(BST.root,15));
