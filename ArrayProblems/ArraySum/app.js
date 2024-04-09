5140a3b13e4d6eaf5d4d6e506c9a122fdc56f053
Amir Ali anwar <amiralianwar611@gmail.com>
2024-04-09 14:12:48 +0500

    check triangle challange completed

--- ArrayProblems/ArraySum/app.js
+++ ArrayProblems/ArraySum/app.js
@@ -653,4 +653,18 @@ const countChar = (str, char) => {
   return totalCount
 }
 
-console.log(countChar('yahoo','i'));
+// console.log(countChar('yahoo','i'));
+
+
+//  Write a function called checkTriangleType that takes three parameters representing the  lengths of the sides of a triangle
+// The function should return a string indicating the type of triangle 'equilateral','isosceles' or 'scalene'
+
+const checkTriangleType=(a,b,c)=>{
+  if (a===null || b==null || c==null) throw new Error("Please enter all the values")
+  if(a === b && b==c) return 'equilateral'
+  if (a == b || b == c || a === c) return "isosceles"
+  return  'scalene'
+}
+
+console.log(checkTriangleType(2,2,2));
+console.log(checkTriangleType(0,0,0));
