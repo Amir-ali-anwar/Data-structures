8e40676488a739c8659eabd6bf56ccd5cc8495f4
Amir Ali anwar <amiralianwar611@gmail.com>
2024-04-17 22:11:54 +0500

    array problem solved

--- ArrayProblems/ArraySum/app.js
+++ ArrayProblems/ArraySum/app.js
@@ -883,8 +883,51 @@ const areEqual = (a, b) => {
   return result
 } 
 
-const smallArray = [2, 3];
-const smallArray1 = [2, 3];
-const largeArray= Array.from({length:100},(_,i)=>i)
-console.log(largeArray);
-console.log(areEqual(smallArray,largeArray));
+const smallArray = [2, 3,500];
+const smallArray1 = [2,3,500];
+const largeArray= Array.from({length:501},(_,i)=>i)
+// console.log(areEqual(smallArray,smallArray1));
+
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+// int ar[n]: an array of integers .
+// Return
+
+// long: the sum of all array elements
+// Input Format
+
+// The first line of the input consists of an integer .
+// The next line contains  space-separated integers contained in the array.
+
+// Output Format
+
+// Return the integer sum of the elements in the array.
+
+// Constraints
+
+
+// Sample Input
+
+// 5
+// 1000000001 1000000002 1000000003 1000000004 1000000005
+// Output
+
+// 5000000015
+// Note:
+
+// The range of the 32-bit integer is .
+// When we add several integer values, the resulting sum might exceed the above range. You might need to use long int C/C++/Java to store such sums.
+
+
+const aVeryBigSum=(arr)=>{
+    
+  return arr
+}
+
+const inputSanple="1000000001 1000000002 1000000003 1000000004 1000000005"
+console.log(aVeryBigSum(inputSanple));
