bc5d7e3ff1bb94f625dec7c625c3559a10d5e20f
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-05-08 22:51:35 +0500

    Problem solved

--- ArrayProblems/ArraySum/app.js
+++ ArrayProblems/ArraySum/app.js
@@ -1423,3 +1423,14 @@ console.log(gradingStudents(grades));
 function nextMultipleOf5(number) {
   return Math.ceil(number / 5) * 5;
 }
+
+// write a function to check if a character is uppercase or lowercase
+
+const isUpperCase = (char) => {
+  if (char.charCodeAt(0) > 65 && char.charCodeAt(0) < 90) {
+    return true
+  }
+  return false
+};
+
+console.log(isUpperCase('DATA'));
