177905e15c59bae19e6aa9b414f7b795cc9f9788
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-06-07 22:26:56 +0500

    Problem solving

--- ArrayProblems/ArraySum/app.js
+++ ArrayProblems/ArraySum/app.js
@@ -1819,6 +1819,7 @@ function breakingRecords(scores) {
   let minCount = 0;
   let maxCount = 0;
   for (let i = 0; i < scores.length; i++) {
+    console.log(scores[i] > maxScore);
     if (scores[i] > maxScore) {
       maxScore = scores[i];
       maxCount++;
@@ -1832,3 +1833,58 @@ return [maxCount,minCount]
 
 let scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
 console.log(breakingRecords(scores)); // Output: [2, 4]
+
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+
+
+function twoSum(nums, target) {
+  let result = [];
+  let numMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement= target-nums[i]
+    if(numMap.has(complement)){
+      return [numMap.get(complement),i]
+    }
+    numMap.set(nums[i],i)
+  }
+  return [];
+}
+
+
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+let nums1 = [3,2,3];
+let target1 = 6;
+console.log(twoSum(nums1, target1));
