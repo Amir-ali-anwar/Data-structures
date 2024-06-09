a4f89f8b43d5d55fb4ba8be96d51ba484d850310
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-06-09 13:50:54 +0500

    Problem solved

--- ArrayProblems/ArraySum/app.js
+++ ArrayProblems/ArraySum/app.js
@@ -1888,3 +1888,55 @@ let target = 9;
 let nums1 = [3,2,3];
 let target1 = 6;
 console.log(twoSum(nums1, target1));
+
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+// Example 1:
+
+
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+// Example 2:
+
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+// Example 3:
+
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+ 
+
+// Constraints:
+
+// The number of nodes in each linked list is in the range [1, 100].
+// 0 <= Node.val <= 9
+// It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+const addTwoNumbers = (l1, l2) => {
+  let arraySum = [];
+  let carry = 0;
+  let maxLength = Math.max(l1.length, l2.length);
+  for (let i = 0; i < maxLength; i++) {
+    let digit = i < l1.length ? l1[i] : 0;
+    let digit2 = i < l2.length ? l2[i] : 0;
+    let arraysSum = digit + digit2;
+    carry = Math.floor(arraysSum / 10);
+    arraySum.push(arraysSum % 10);
+  }
+  return arraySum;
+};
+
+const l1 = [1, 2, 3]; 
+const l2 = [4, 5, 6];
+
+const l3 = [2, 4]; 
+const l4 = [5, 5, 4];
+console.log(addTwoNumbers(l3,l4));
+
