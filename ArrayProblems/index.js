1af099fa3aeff33a3f44564e991e5d47354976cc
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-06-19 16:41:17 +0500

    Problem solved

--- ArrayProblems/index.js
+++ ArrayProblems/index.js
@@ -3,7 +3,7 @@
 
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
- 
+
 
 // Example 1:
 
@@ -19,7 +19,7 @@
 
 // Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 // Output: [8,9,9,9,0,0,0,1]
- 
+
 
 // Constraints:
 
@@ -31,57 +31,57 @@
 // via Linklist
 
 
-    class NodeList {
-        constructor(value = 0, next = null) {
-          this.value = value;
-          this.next = next;
-        }
-      }
-    function arrayToList(arr) {
+class NodeList {
+  constructor(value = 0, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+function arrayToList(arr) {
 
-      const head = new NodeList();
-      let current = head
-      for (let val of arr) {
-        current.next = new NodeList(val);
-        current = current.next;
-      }
-      return head.next;
-    }
-    function listToArray(list) {
-      let array = []
-      while (list !== null) {
-        array.push(list.val);
-        list = list.next;
-      }
-      return array;
-    }
-    function addNumbers(l1,l2){
-      let dummyHead = new ListNode(0);
-      let p = l1, q = l2, current = dummyHead;
-      let carry = 0;
-      while (p !== null || q !== null) {
-        let x = (p !== null) ? p.val : 0;
-        let y = (q !== null) ? q.val : 0;
-        let sum = carry + x + y;
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
-        if (p !== null) p = p.next;
-        if (q !== null) q = q.next;
-    }
+  const head = new NodeList();
+  let current = head
+  for (let val of arr) {
+    current.next = new NodeList(val);
+    current = current.next;
+  }
+  return head.next;
+}
+function listToArray(list) {
+  let array = []
+  while (list !== null) {
+    array.push(list.val);
+    list = list.next;
+  }
+  return array;
+}
+function addNumbers(l1, l2) {
+  let dummyHead = new ListNode(0);
+  let p = l1, q = l2, current = dummyHead;
+  let carry = 0;
+  while (p !== null || q !== null) {
+    let x = (p !== null) ? p.val : 0;
+    let y = (q !== null) ? q.val : 0;
+    let sum = carry + x + y;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    if (p !== null) p = p.next;
+    if (q !== null) q = q.next;
+  }
 
-      if (carry > 0) {
-        current.next = new ListNode(carry);
-    }
-    return dummyHead.next;
-    }
-  let list = [2, 7, 11, 15];
-  // console.log(arrayToList(list));
-
-
+  if (carry > 0) {
+    current.next = new ListNode(carry);
+  }
+  return dummyHead.next;
+}
+let list = [2, 7, 11, 15];
+// console.log(arrayToList(list));
 
 
-  // Given a string s, find the length of the longest substring without repeating characters.
+
+
+// Given a string s, find the length of the longest substring without repeating characters.
 
 //   Example 1:
 
@@ -106,7 +106,7 @@ function lengthOfLongestSubstring(tr) {
   for (let i = 0; i < trIntoArr.length; i++) {
     for (let j = trIntoArr.length; j > i; j--) { // Corrected the ending index
       let substring = tr.substring(i, j);
-      if(isValidSubstring(substring)){
+      if (isValidSubstring(substring)) {
         maxLength = Math.max(maxLength, substring.length);
         break;
       }
@@ -127,4 +127,26 @@ function isValidSubstring(substring) {
 }
 const inputString = "abcabcbb";
 
-console.log(lengthOfLongestSubstring(inputString));
+// console.log(lengthOfLongestSubstring(inputString));
+
+//v2
+
+
+function lengthOfLongestSubstringV2(Str) {
+  let maxLength = 0;
+  let left = 0;
+  let charMap = new Map()
+  for (let right = 0; right < Str.length; right++) {
+    let currentCharacter = Str.charAt(right)
+    console.log(currentCharacter);
+    if (charMap.has(currentCharacter)) {
+      charMap.delete(Str.charAt(left));
+      left++
+    }
+    charMap.set(currentCharacter);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength
+}
+
+console.log(lengthOfLongestSubstringV2(inputString));
