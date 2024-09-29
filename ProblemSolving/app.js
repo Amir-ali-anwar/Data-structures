ea30c1dd4a9f5db38f6c729d16c66a5cda8f0d34
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-09-29 11:51:28 +0500

    Problem solved

--- ProblemSolving/app.js
+++ ProblemSolving/app.js
@@ -153,3 +153,95 @@ function birthday(s, d, m) {
 console.log(birthday([1, 2, 1, 3, 2], 3, 2));  // Output: 2
 console.log(birthday([1, 1, 1, 1, 1, 1], 3, 2));  // Output: 0
 console.log(birthday([4], 4, 1));  // Output: 1
+
+
+
+// Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+
+// Example
+
+// There are two each of types  and , and one sighting of type . Pick the lower of the two types seen twice: type .
+
+// Function Description
+
+// Complete the migratoryBirds function in the editor below.
+
+// migratoryBirds has the following parameter(s):
+
+// int arr[n]: the types of birds sighted
+// Returns
+
+// int: the lowest type id of the most frequently sighted birds
+// Input Format
+
+// The first line contains an integer, , the size of .
+// The second line describes  as  space-separated integers, each a type number of the bird sighted.
+
+// Constraints
+
+// It is guaranteed that each type is , , , , or .
+// Sample Input 0
+
+// 6
+// 1 4 4 4 5 3
+// Sample Output 0
+
+// 4
+// Explanation 0
+
+// The different types of birds occur in the following frequencies:
+
+// Type :  bird
+// Type :  birds
+// Type :  bird
+// Type :  birds
+// Type :  bird
+// The type number that occurs at the highest frequency is type , so we print  as our answer.
+
+// Sample Input 1
+
+// 11
+// 1 2 3 4 5 4 3 2 1 3 4
+// Sample Output 1
+
+// 3
+// Explanation 1
+
+// The different types of birds occur in the following frequencies:
+
+// Type : 
+// Type : 
+// Type : 
+// Type : 
+// Type : 
+// Two types have a frequency of , and the lower of those is type .
+
+// Object.keys(console).forEach(method => console[method] = () => {});
+function migratoryBirds(arr) {
+  const birdCount = {}; 
+  for (let i = 0; i < arr.length; i++) {
+    birdCount[arr[i]]=(birdCount[arr[i]]|| 0)+1
+  }
+  console.log(birdCount);
+  
+  let maxCount = 0;
+  let mincount= Number.MIN_SAFE_INTEGER
+  for (const bird in birdCount) {
+    const count = birdCount[bird];
+    if(count > maxCount){
+      maxCount=count
+      mincount=bird
+    }
+    else if (count === maxCount) {
+      mincount = Math.min(mincount, parseInt(bird));
+    }
+  }
+ return mincount
+  
+}
+
+const bird_sightings = [1, 2, 3 ,4 ,5 ,4 ,3 ,2, 1, 3, 4];
+const bird_sightings_1 = [1, 4 ,4 ,4, 5, 3
+];
+console.log(migratoryBirds(bird_sightings_1));  // Output: 4
+
