8dfd8155a5c0b793da4722681eebc9f0d768ff5b
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-10-13 21:59:29 +0500

    problem solved

--- ProblemSolving/app.js
+++ ProblemSolving/app.js
@@ -245,3 +245,70 @@ const bird_sightings_1 = [1, 4 ,4 ,4, 5, 3
 ];
 console.log(migratoryBirds(bird_sightings_1));  // Output: 4
 
+
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+// Example
+
+// The minimum sum is  and the maximum sum is . The function prints Function Description
+
+// Complete the miniMaxSum function in the editor below.
+
+// miniMaxSum has the following parameter(s):
+
+// arr: an array of  integers
+// Print
+
+// Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+// Input Format
+
+// A single line of five space-separated integers.
+
+// Constraints
+
+
+// Output Format
+
+// Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+// Sample Input
+
+// 1 2 3 4 5
+// Sample Output
+
+// 10 14
+// Explanation
+
+// The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Sum everything except , the sum is .
+// Hints: Beware of integer overflow! Use 64-bit Integer.
+
+
+
+function miniMaxSum(arr) {
+  // Write your code here
+  let sum = 0
+  let sumArray = []
+  for (let i = 0; i < arr.length; i++) {
+     sum += arr[i]
+  }
+  for (const key of arr) {
+
+     sumArray.push(sum - key)
+
+  }
+   const minSum = Math.min(...sumArray);
+  const maxSum = Math.max(...sumArray);
+  console.log(minSum,maxSum);
+
+}
+
+const minmax = [1, 3, 5, 7, 9]
+
+console.log(miniMaxSum(minmax));
