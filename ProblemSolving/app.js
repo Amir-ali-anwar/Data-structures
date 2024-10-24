b15a81d5c225329969519a37ff486aced35b50a1
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-10-24 11:12:01 +0500

    Problem solved

--- ProblemSolving/app.js
+++ ProblemSolving/app.js
@@ -312,3 +312,65 @@ function miniMaxSum(arr) {
 const minmax = [1, 3, 5, 7, 9]
 
 console.log(miniMaxSum(minmax));
+
+
+
+// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+// Example
+
+
+// Return '12:01:00'.
+
+
+// Return '00:01:00'.
+
+// Function Description
+
+// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+// timeConversion has the following parameter(s):
+
+// string s: a time in  hour format
+// Returns
+
+// string: the time in  hour format
+// Input Format
+
+// A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+// Constraints
+
+// All input times are valid
+// Sample Input
+
+// 07:05:45PM
+// Sample Output
+
+// 19:05:45
+
+
+function timeConversion(s) {
+  const timeConversion = s.slice(-2)
+  let timewithoutFormat = s.slice(0, -2)
+  let [hours, minutes, seconds] = timewithoutFormat.split(':')
+  if (timeConversion === 'AM') {
+    if (hours === '12') {
+      hours = '00'; // Midnight case
+    }
+  }
+  else if (timeConversion === 'PM') {
+    if (hours !== '12') {
+      hours = String(parseInt(hours) + 12); // Convert to 24-hour format
+    }
+  }
+  return `${hours}:${minutes}:${seconds}`;
+
+}
+
+let timeInput = "07:05:45PM";
+
+console.log(timeConversion(timeInput));
