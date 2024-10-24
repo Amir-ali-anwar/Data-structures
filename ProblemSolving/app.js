8dfa688423289f3572fc72e596afdd1a7a082696
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-10-24 12:27:42 +0500

    Problem solved

--- ProblemSolving/app.js
+++ ProblemSolving/app.js
@@ -374,3 +374,38 @@ function timeConversion(s) {
 let timeInput = "07:05:45PM";
 
 console.log(timeConversion(timeInput));
+
+
+// Given an array of integers, where all elements but one occur twice, find the unique element.
+
+// Example
+// a=[1,2,3,4,3,2,1]
+
+
+// The unique element is .
+
+// Function Description
+
+// Complete the lonelyinteger function in the editor below.
+
+// lonelyinteger has the following parameter(s):
+
+// int a[n]: an array of integers
+// Returns
+
+// int: the element that occurs only once
+// Input Format
+
+// The first line contains a single integer, , the number of integers in the array.
+// The second line contains  space-separated integers that describe the values in .
+
+
+function lonelyinteger(a) {
+  let uniqueElement = 0;
+  for (const element of a) {
+      uniqueElement ^= element
+  }
+  return uniqueElement
+}
+const a=[1,2,3,4,3,2,1]
+console.log(lonelyinteger(a));
