041aa0c6ed718e10619e372919fef01580d26781
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-01-06 15:42:38 +0500

    Longest Common Prefix Problem solved

--- Quick-preparation/app.js
+++ Quick-preparation/app.js
@@ -887,4 +887,45 @@ let romanInput= "MCMXCIV"
 
 console.log(RomanToInt(romanInput));
 
+// Longest Common Prefix
 
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+ 
+
+// Constraints:
+
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] consists of only lowercase English letters.
+
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return ''
+    let base = strs[0]
+    for (let i = 0; i < base.length; i++) {
+        for (let j = 0; j < strs.length; j++) {
+            if (i >= strs[j].length || base[i] !== strs[j][i]) {
+               return base.slice(0, i)
+            }
+        }
+    }
+    return base
+}
+
+let strs = ["flower", "flow", "flight"]
+
+console.log(longestCommonPrefix(strs));
