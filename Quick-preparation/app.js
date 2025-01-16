7b9f0aff22156c3245d59a4987096181da26b1f9
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-01-16 22:53:41 +0500

    Problem solving

--- Quick-preparation/app.js
+++ Quick-preparation/app.js
@@ -1282,3 +1282,70 @@ let callCount = 0;
   memoizedFn(2, 3) // 5
   memoizedFn(2, 3) // 5
  console.log({callCount})
+
+
+//  Add Two Promises
+
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+
+// Example 1:
+
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+// Output: 7
+// Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+
+// Example 2:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+// Output: -2
+// Explanation: The two input promises resolve with the values of 10 and -12 respectively. The returned promise should resolve with a value of 10 + -12 = -2.
+
+const addTwoPromises = async function (promise1, promise2) {
+    return Promise.all([promise1, promise2]).then(([result1,result2]) => {
+        return result1 + result2
+    }).catch((err) => {
+        console.log(err);
+        throw new Error(err)
+    })
+};
+
+ addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+   .then(console.log); // 4
+
+
+//  Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn); 
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+ 
+
+// Constraints:
+
+// calls is a valid JSON array
+// 1 <= calls.length <= 10
+// 1 <= calls[i].length <= 100
+// 2 <= JSON.stringify(calls).length <= 1000
