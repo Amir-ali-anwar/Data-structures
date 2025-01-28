03199329e4118882481d40a6614b7b8f4c28ba0c
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-01-28 23:08:47 +0500

    Wip:Problem solving

--- Quick-preparation/app.js
+++ Quick-preparation/app.js
@@ -1712,3 +1712,42 @@ const unsortedArray = [-4, -1, 0, 3, 10]
 
 
 console.log(sortedSquares(unsortedArray));
+
+
+// Find the Index of the First Occurrence in a String
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+const strStr = function (haystack, needle) {
+    if (needle.length === 0) return 0
+    let left = 0;
+    let right = needle.length;
+    console.log(haystack[left]=== needle[right]);
+    
+    // while (left <= right) {
+    //     if (haystack[left]=== needle[right]) {
+    //         left++
+    //         return left
+    //     } 
+    // }
+    // return -1
+};
+
+
+const haystack ="sadbutsad"
+const needle ="sad"
+console.log(strStr(haystack,needle));
