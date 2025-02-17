0ec5c486e9e433f61bfd1c0b356e39a70fd2177e
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-02-17 17:22:57 +0500

    Problem soolving

--- Quick-preparation/app.js
+++ Quick-preparation/app.js
@@ -1888,3 +1888,43 @@ const target1= 9;
 console.log(search(SortedArray,target1));
 
 
+//35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+const searchInsert = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] > target) {
+            right = mid - 1
+        } else if (nums[mid] < target) {
+            left = mid + 1
+
+        } else {
+            return left
+        }
+    }
+    return -1
+};
+const numsv3=[1,3,5,6]
+const targetv3= 1;
+
+console.log(searchInsert(numsv3,targetv3));
