1c1fee5f50947d1226ea039627df4e58ce8fba11
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-02-21 15:06:04 +0500

    Solving problems

--- SlidingWindow/app.js
+++ SlidingWindow/app.js
@@ -20,4 +20,35 @@ const findMaxAverage = function (nums, k) {
 
 const _subArray = [1,12,-5,-6,50,3];
 const k = 4;
-console.log(findMaxAverage(_subArray, k));
+// console.log(findMaxAverage(_subArray, k));
+
+
+
+// Maximum Sum Subarray of Size K (easy)
+
+// You are given an array of integers nums and an integer k. Your task is to find the maximum sum of any contiguous subarray of size k.]
+
+const maxSumSubarrayOfSizeK=(nums,_kv2)=>{
+  let maxSum = -Infinity;
+  let windowSum = 0;
+  for (let i = 0; i <_kv2; i++) {
+    windowSum += nums[i];
+  }
+  
+  maxSum = windowSum;
+  for (let i=_kv2; i < nums.length; i++) {
+    // windowSum = windowSum + newElement - oldElement
+    windowSum = windowSum  + nums[i] - nums[i - _kv2];
+    maxSum = Math.max(maxSum, windowSum);
+  }
+  return maxSum;
+}
+
+const _subArrayv2 = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
+const _kv2=3
+console.log(maxSumSubarrayOfSizeK(_subArrayv2,_kv2));
+
+
+
+
+
