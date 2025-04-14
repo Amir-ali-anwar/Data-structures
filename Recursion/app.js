ed1ddb01d3c0109c13f4e2eb1e48ddee4d775d18
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-14 09:34:22 +0500

    Solving problems

--- Recursion/app.js
+++ Recursion/app.js
@@ -12,21 +12,39 @@ function printNumbers(n) {
 
 // console.log(printNumbers(5));
 
-
 // Print 1 to N (decreasing order)
-function printNumbersDecreasing(n,isIncreasing=false) {
-    if (n === 1) {
-      console.log(n);
-      return n;
-    }
-    if (isIncreasing) {
-        printNumbers(n - 1, isIncreasing);
-        console.log(n);
-      } else {
-        console.log(n);
-        printNumbers(n - 1, isIncreasing); 
-      }
+function printNumbersDecreasing(n, isIncreasing = false) {
+  if (n === 1) {
+    console.log(n);
+    return n;
   }
-  
-  console.log(printNumbersDecreasing(5,false));
-  
+  if (isIncreasing) {
+    printNumbers(n - 1, isIncreasing);
+    console.log(n);
+  } else {
+    console.log(n);
+    printNumbers(n - 1, isIncreasing);
+  }
+}
+
+// console.log(printNumbersDecreasing(5, false));
+
+// Print sum of first N natural numbers using recursion
+
+function PrintSum(n) {
+  if (n === 1 || n === 0 || n < 1) {
+    // console.log(n);
+    return Math.abs(n);
+  }
+//   if (n === 0) return n;
+  return PrintSum(n - 1) + n;
+}
+
+console.log(PrintSum(12));
+
+
+function PrintSumV2(n) {
+    return (n * (n + 1)) / 2;
+  }
+
+console.log(PrintSumV2(1222222));
