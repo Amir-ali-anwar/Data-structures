bb596fc698737d051715751040f65951cb157dce
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-05-12 10:30:26 +0500

    Problem solving

--- TopInterview150/app.js
+++ TopInterview150/app.js
@@ -369,3 +369,48 @@ const romanToInt = function (romanInput) {
 const s = "III";
 
 // console.log(romanToInt(s));
+
+
+// 28. Find the Index of the First Occurrence in a String
+
+
+
+  // Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+  
+
+  // Example 1:
+
+  // Input: haystack = "sadbutsad", needle = "sad"
+  // Output: 0
+  // Explanation: "sad" occurs at index 0 and 6.
+  // The first occurrence is at index 0, so we return 0.
+  // Example 2:
+
+  // Input: haystack = "leetcode", needle = "leeto"
+  // Output: -1
+  // Explanation: "leeto" did not occur in "leetcode", so we return -1.
+  
+
+  // Constraints:
+
+  // 1 <= haystack.length, needle.length <= 104
+  // haystack and needle consist of only lowercase English characters.
+
+
+  const strStr = function (haystack, needle) {
+    if (needle.length === 0) return 0;
+    for (let left = 0; left < haystack.length - needle.length; left++) {
+      let right = left + needle.length;
+      if (haystack.slice(left, right) === needle) {
+        return haystack.slice(left, right);
+      }
+    }
+    return -1;
+  };
+
+  const haystack = "sadbutsad";
+  const needle = "sad";
+
+  console.log(strStr(haystack, needle));
+  
