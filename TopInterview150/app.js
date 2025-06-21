5a8fc739c25e35c2f48fe8a0537c4d8763948b38
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-06-21 16:26:55 +0500

    Problem solved

--- TopInterview150/app.js
+++ TopInterview150/app.js
@@ -539,12 +539,42 @@ const mySqrt = (x) => {
 // digits does not contain any leading 0's.
 
 
+//Container With Most Water
+
+
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container
+
+
+
+
+const maxArea = (height) => {
+  let left = 0;
+  let right = height.length - 1;
+  let maxheight = 0;
+  while (left < right) {
+    let area = Math.min(height[left], height[right]) * (right - left);
+    maxheight = Math.max(area, maxheight);
+    if (height[left] > height[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
 
 
 
 
 
 
+
+const height= [1,8,6,2,5,4,8,3,7]
 
 
 
